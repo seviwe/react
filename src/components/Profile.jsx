aeab17f8156fd5a0af370247dc40a8891d6d43d9
Seviwe_Work <aleinikov@CENTER3.lan.mivlgu.ru>
2020-03-03 16:10:57 +0300

    First commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://ic.pics.livejournal.com/fanatbaikala/61253622/44374/44374_original.jpg" />
+      </div>
+      <div>
+        avatar + desc
+   </div>
+      <div>
+        My posts
+     <div>
+          New post
+     </div>
+        <div>
+          post 1
+     </div>
+        <div>
+          post 2
+     </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
