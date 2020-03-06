b54127c8684aa675c258bc6c3299314cc20a0b87
Seviwe_Work <aleinikov@CENTER3.lan.mivlgu.ru>
2020-03-06 13:46:06 +0300

    lessons 19 browser-router

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import Profile from './components/Profile/Profile';
 import NavbarFriends from './components/NavbarFriends/NavbarFriends';
 import Dialogs from './components/Dialogs/Dialogs';
 import Footer from './components/Footer/Footer';
+import { Route, BrowserRouter } from "react-router-dom"
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <BackgroundImage />
-      <Navbar />
-      <NavbarFriends />
-      <div className="app-wrapper-content">
-          {/* <Profile /> */}
-          <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <BackgroundImage />
+        <Navbar />
+        <NavbarFriends />
+        <div className="app-wrapper-content">
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/settings' component={Settings} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
