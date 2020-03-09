c07a733770dfc18a49648d96d8722b741fcae8c8
SEVA@HOME <sevaaleynikov@home.com>
2020-03-09 16:57:39 +0300

    lesson 21 create page dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,33 @@ import styles from './Dialogs.module.css';
 
 const Dialogs = () => {
 	return (
-		<div>
-			Dialogs
-      </div>
+		<div className={styles.dialogs}>
+			<div className={styles.dialogsItems}>
+				<div className={styles.dialog + ' ' + styles.active}>
+					Дмитрий
+				</div>
+				<div className={styles.dialog}>
+					Светлана
+				</div>
+				<div className={styles.dialog}>
+					Женя
+				</div>
+				<div className={styles.dialog}>
+					Андрей
+				</div>
+				<div className={styles.dialog}>
+					Александр
+				</div>
+				<div className={styles.dialog}>
+					Мария
+				</div>
+			</div>
+			<div className={styles.messages}>
+				<div className={styles.message}>Привет!</div>
+				<div className={styles.message}>Как дела?</div>
+				<div className={styles.message}>Чем занимаешься?</div>
+			</div>
+		</div>
 	)
 }
 
