dc688272ceffffa574f61bcaa60b25db2f758b7f
Seviwe_Work <aleinikov@CENTER3.lan.mivlgu.ru>
2020-03-18 16:35:24 +0300

    Правка верстки

--- src/components/Profile/MyPosts/Posts/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styles from './Post.module.css';
 
 const Post = (props) => {
-  return (
-    <div className={styles.item}>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZH7c1xxmppduvnIzq7oevpcDCK3nDoGgqjR6-yZ7L7kHCKDeq" />
-      {props.message}
-
-      <div className={styles.LikeDislike}>
-        <span>Like ({props.countLike}) </span>
-        <span>Dislike ({props.countDislike})</span>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.item}>
+			<div>
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZH7c1xxmppduvnIzq7oevpcDCK3nDoGgqjR6-yZ7L7kHCKDeq" />
+			</div>
+			<div>
+				<div className={styles.postMessage}>
+					{props.message}
+				</div>
+				<div className={styles.LikeDislike}>
+					<span>Like ({props.countLike}) </span>
+					<span>Dislike ({props.countDislike})</span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Post;
