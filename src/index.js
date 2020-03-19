83217807e5d463612fc39787734414afc8be93a4
Seviwe_Work <aleinikov@CENTER3.lan.mivlgu.ru>
2020-03-19 16:48:09 +0300

    37 создание storе в state.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe, addPost, updateNewPostText, addMessage, updateNewMessageText } from './redux/state'
+import store from './redux/state'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,13 +10,13 @@ import { BrowserRouter } from 'react-router-dom';
 let rerender = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
+            <App state={store.getState()} addPost={store.addPost} updateNewPostText={store.updateNewPostText} addMessage={store.addMessage} updateNewMessageText={store.updateNewMessageText} />
         </BrowserRouter>, document.getElementById('root'));
 }
 
-rerender(state);
+rerender(store.getState());
 
-subscribe(rerender);
+store.subscribe(rerender);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,84 +1,87 @@
-let rerender = () => { }
 
-let avatarImage = {
-    imgs: [
-        { id: 1, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFD1Ofi7-DsfgVE7CojIqMuNGYEN1N4dGyec3hJQebtISancyF' },
-        { id: 2, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTY8JBBDJ15r08pjcZ81aWOEgFGPKLV-yXEz_9LxMk7Dn_-SYOg' },
-        { id: 3, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPg3XBAD7Y_OBK42lSSsE6EmstHQFwDhos9-_U7qalNs8azhSk' },
-        { id: 4, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKs6g39CeptLQsVJ2m_w7Ib6RsPCuJuUsUsMq9aafQGVB9hhsg' },
-        { id: 5, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmHvlQey7sRB-lIKvwZQHlY-Gwi0TIDWloz6LZcCYwdubZ5-nV' },
-    ]
-}
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hello World!', countLike: 20, countDislike: 6 },
-            { id: 2, message: 'It\'s my first post.', countLike: 123, countDislike: 56 },
-            { id: 3, message: 'Test meesage', countLike: 500, countDislike: 0 },
-            { id: 4, message: 'Test meesage 2', countLike: 0, countDislike: 0 },
-        ],
-        newPostText: ''
-    },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Дмитрий', imgSrc: avatarImage.imgs[0].src },
-            { id: 2, name: 'Светлана', imgSrc: avatarImage.imgs[1].src },
-            { id: 3, name: 'Женя', imgSrc: avatarImage.imgs[3].src },
-            { id: 4, name: 'Андрей', imgSrc: avatarImage.imgs[4].src },
-            { id: 5, name: 'Александр', imgSrc: avatarImage.imgs[2].src },
-            { id: 6, name: 'Мария', imgSrc: avatarImage.imgs[1].src },
-        ],
-        messages: [
-            { id: 1, message: 'Привет!' },
-            { id: 2, message: 'Как дела?' },
-            { id: 3, message: 'Чем занимаешься?' },
-        ],
-        newMessageText: ''
-    },
-    friendsPage: {
-        friends: [
-            { id: 1, imgSrc: avatarImage.imgs[0].src, name: 'Михаил', surname: 'Павлов' },
-            { id: 2, imgSrc: avatarImage.imgs[3].src, name: 'Дмитрий', surname: 'Иванов' },
-            { id: 3, imgSrc: avatarImage.imgs[1].src, name: 'Светлана', surname: 'Ясная' },
-            { id: 4, imgSrc: avatarImage.imgs[1].src, name: 'Елена', surname: 'Гребникова' },
-            { id: 5, imgSrc: avatarImage.imgs[2].src, name: 'Женя', surname: 'Казанский' },
-            { id: 6, imgSrc: avatarImage.imgs[4].src, name: 'Кирилл', surname: 'Эльман' },
+let store = {
+    _avatarImage: {
+        imgs: [
+            { id: 1, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFD1Ofi7-DsfgVE7CojIqMuNGYEN1N4dGyec3hJQebtISancyF' },
+            { id: 2, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTY8JBBDJ15r08pjcZ81aWOEgFGPKLV-yXEz_9LxMk7Dn_-SYOg' },
+            { id: 3, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPg3XBAD7Y_OBK42lSSsE6EmstHQFwDhos9-_U7qalNs8azhSk' },
+            { id: 4, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKs6g39CeptLQsVJ2m_w7Ib6RsPCuJuUsUsMq9aafQGVB9hhsg' },
+            { id: 5, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmHvlQey7sRB-lIKvwZQHlY-Gwi0TIDWloz6LZcCYwdubZ5-nV' },
         ]
     },
+    getAvatarImage(index) {
+        return this._avatarImage.imgs[index].src;
+    },
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hello World!', countLike: 20, countDislike: 6 },
+                { id: 2, message: 'It\'s my first post.', countLike: 123, countDislike: 56 },
+                { id: 3, message: 'Test meesage', countLike: 500, countDislike: 0 },
+                { id: 4, message: 'Test meesage 2', countLike: 0, countDislike: 0 },
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Дмитрий', imgSrc: this.getAvatarImage(0) },
+                { id: 2, name: 'Светлана', imgSrc: this.getAvatarImage(1) },
+                { id: 3, name: 'Женя', imgSrc: this.getAvatarImage(3) },
+                { id: 4, name: 'Андрей', imgSrc: this.getAvatarImage(4) },
+                { id: 5, name: 'Александр', imgSrc: this.getAvatarImage(2) },
+                { id: 6, name: 'Мария', imgSrc: this.getAvatarImage(1) },
+            ],
+            messages: [
+                { id: 1, message: 'Привет!' },
+                { id: 2, message: 'Как дела?' },
+                { id: 3, message: 'Чем занимаешься?' },
+            ],
+            newMessageText: ''
+        },
+        friendsPage: {
+            friends: [
+                { id: 1, imgSrc: this.getAvatarImage(0), name: 'Михаил', surname: 'Павлов' },
+                { id: 2, imgSrc: this.getAvatarImage(3), name: 'Дмитрий', surname: 'Иванов' },
+                { id: 3, imgSrc: this.getAvatarImage(1), name: 'Светлана', surname: 'Ясная' },
+                { id: 4, imgSrc: this.getAvatarImage(1), name: 'Елена', surname: 'Гребникова' },
+                { id: 5, imgSrc: this.getAvatarImage(2), name: 'Женя', surname: 'Казанский' },
+                { id: 6, imgSrc: this.getAvatarImage(4), name: 'Кирилл', surname: 'Эльман' },
+            ]
+        },
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() { },
+    //функция добавления поста на стену
+    addPost() {
+        let newPost = { id: 5, message: this._state.profilePage.newPostText, countLike: 0, countDislike: 0 };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    //функция обновления текста в textarea
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    //функция добавления поста на стену
+    addMessage() {
+        let newMessage = { id: 4, message: this._state.dialogsPage.newMessageText };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    //функция обновления текста в textarea
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    //наблюдатель
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-//функция добавления поста на стену
-export const addPost = () => {
-    let newPost = { id: 5, message: state.profilePage.newPostText, countLike: 0, countDislike: 0 };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerender(state);
-}
-
-//функция обновления текста в textarea
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerender(state);
-}
-
-//функция добавления поста на стену
-export const addMessage = () => {
-    let newMessage = { id: 4, message: state.dialogsPage.newMessageText };
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerender(state);
-}
-
-//функция обновления текста в textarea
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerender(state);
-}
-
-//наблюдатель
-export const subscribe = (observer) => {
-    rerender = observer;
-}
-
-export default state;
+export default store;
