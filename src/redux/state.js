90ba68edce500063ad43a207ecc601c08636c188
Seviwe_Work <aleinikov@CENTER3.lan.mivlgu.ru>
2020-03-20 13:39:25 +0300

    исправление state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,6 @@
-
-
-let store = {
-    _avatarImage: {
-        imgs: [
+let avatarImage = {
+    _image: {
+        img: [
             { id: 1, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFD1Ofi7-DsfgVE7CojIqMuNGYEN1N4dGyec3hJQebtISancyF' },
             { id: 2, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTY8JBBDJ15r08pjcZ81aWOEgFGPKLV-yXEz_9LxMk7Dn_-SYOg' },
             { id: 3, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPg3XBAD7Y_OBK42lSSsE6EmstHQFwDhos9-_U7qalNs8azhSk' },
@@ -10,9 +8,12 @@ let store = {
             { id: 5, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmHvlQey7sRB-lIKvwZQHlY-Gwi0TIDWloz6LZcCYwdubZ5-nV' },
         ]
     },
-    getAvatarImage(index) {
-        return this._avatarImage.imgs[index].src;
+    getAvatarImage(value) {
+        return this._image.img[value].src;
     },
+}
+
+let store = {
     _state: {
         profilePage: {
             posts: [
@@ -25,12 +26,12 @@ let store = {
         },
         dialogsPage: {
             dialogs: [
-                { id: 1, name: 'Дмитрий', imgSrc: this.getAvatarImage(0) },
-                { id: 2, name: 'Светлана', imgSrc: this.getAvatarImage(1) },
-                { id: 3, name: 'Женя', imgSrc: this.getAvatarImage(3) },
-                { id: 4, name: 'Андрей', imgSrc: this.getAvatarImage(4) },
-                { id: 5, name: 'Александр', imgSrc: this.getAvatarImage(2) },
-                { id: 6, name: 'Мария', imgSrc: this.getAvatarImage(1) },
+                { id: 1, name: 'Дмитрий', imgSrc: avatarImage.getAvatarImage(0) },
+                { id: 2, name: 'Светлана', imgSrc: avatarImage.getAvatarImage(1) },
+                { id: 3, name: 'Женя', imgSrc: avatarImage.getAvatarImage(3) },
+                { id: 4, name: 'Андрей', imgSrc: avatarImage.getAvatarImage(4) },
+                { id: 5, name: 'Александр', imgSrc: avatarImage.getAvatarImage(2) },
+                { id: 6, name: 'Мария', imgSrc: avatarImage.getAvatarImage(1) },
             ],
             messages: [
                 { id: 1, message: 'Привет!' },
@@ -41,12 +42,12 @@ let store = {
         },
         friendsPage: {
             friends: [
-                { id: 1, imgSrc: this.getAvatarImage(0), name: 'Михаил', surname: 'Павлов' },
-                { id: 2, imgSrc: this.getAvatarImage(3), name: 'Дмитрий', surname: 'Иванов' },
-                { id: 3, imgSrc: this.getAvatarImage(1), name: 'Светлана', surname: 'Ясная' },
-                { id: 4, imgSrc: this.getAvatarImage(1), name: 'Елена', surname: 'Гребникова' },
-                { id: 5, imgSrc: this.getAvatarImage(2), name: 'Женя', surname: 'Казанский' },
-                { id: 6, imgSrc: this.getAvatarImage(4), name: 'Кирилл', surname: 'Эльман' },
+                { id: 1, imgSrc: avatarImage.getAvatarImage(0), name: 'Михаил', surname: 'Павлов' },
+                { id: 2, imgSrc: avatarImage.getAvatarImage(3), name: 'Дмитрий', surname: 'Иванов' },
+                { id: 3, imgSrc: avatarImage.getAvatarImage(1), name: 'Светлана', surname: 'Ясная' },
+                { id: 4, imgSrc: avatarImage.getAvatarImage(1), name: 'Елена', surname: 'Гребникова' },
+                { id: 5, imgSrc: avatarImage.getAvatarImage(2), name: 'Женя', surname: 'Казанский' },
+                { id: 6, imgSrc: avatarImage.getAvatarImage(4), name: 'Кирилл', surname: 'Эльман' },
             ]
         },
     },
@@ -83,5 +84,7 @@ let store = {
         this._callSubscriber = observer;
     },
 }
+
+console.log(store);
 
 export default store;
