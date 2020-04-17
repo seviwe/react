23a8905acdb0639556256abebdb4b7137e5b70f3
SEVA@HOME <sevaaleynikov@home.com>
2020-04-17 13:35:45 +0300

    lessons 50, 51 REST API теория, Users API подключение, получение пользователей от сервера с помощью библ. Axios

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './User.module.css';
+import noneAvatar from '../../../assets/img/noneAvatar.png';
 
 const User = (props) => {
 	return (
 		<div className={styles.item}>
 			<div>
-				<img src={props.imgSrc} />
+				<img src={props.imgSrc != null ? props.imgSrc : noneAvatar} />
 			</div>
 			<div className={styles.text}>
-				{props.name + ' ' + props.surname}
+				{/* {props.name + ' ' + props.surname} */}
+				{props.name}
 				<div className={styles.textMin}>
 					{props.status}
 				</div>
 				<div className={styles.textMin}>
-					{props.country + ', ' + props.city}
+					{/* {props.country + ', ' + props.city} */}
+					Страна, город
 				</div>
 				<div>
 					{
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './Users.module.css';
 import User from './User/User';
+import * as axios from 'axios';
 
 const Users = (props) => {
-	let usersElement = props.usersPage.users.map(u => <User name={u.name} surname={u.surname} id={u.id} imgSrc={u.imgSrc} status={u.status} country={u.location.country} city={u.location.city} followed={u.followed} follow={props.follow} unfollow={props.unfollow} />);
+
+	if (props.usersPage.users.length === 0) {
+		//запрос с сервера пользователей
+		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+			props.setUsers(response.data.items);
+		});
+	}
+
+	//было раньше до подключения к серверу
+	// let usersElement = props.usersPage.users.map(u => <User name={u.name} surname={u.surname} id={u.id} imgSrc={u.imgSrc} status={u.status} country={u.location.country} city={u.location.city} followed={u.followed} follow={props.follow} unfollow={props.unfollow} />);
+
+	let usersElement = props.usersPage.users.map(u => <User name={u.name} id={u.id} imgSrc={u.photos.small} status={u.status} followed={u.followed} follow={props.follow} unfollow={props.unfollow} />);
 
 	return (
 		<div>
