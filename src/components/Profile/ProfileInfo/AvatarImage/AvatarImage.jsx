63fc1da7254cef0ee6937206b1021b5773ed9414
SEVA@HOME <sevaaleynikov@home.com>
2020-05-04 15:30:52 +0300

    верстка

--- src/components/Profile/ProfileInfo/AvatarImage/AvatarImage.jsx
+++ src/components/Profile/ProfileInfo/AvatarImage/AvatarImage.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import styles from './AvatarImage.module.css';
 
 const AvatarImage = (props) => {
+
+	// if (!props.imgSrc) {
+	// 	return (
+	// 		<div className={styles.avatar}>
+	// 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZH7c1xxmppduvnIzq7oevpcDCK3nDoGgqjR6-yZ7L7kHCKDeq" />
+	// 		</div>
+	// 	)
+	// }
+
 	return (
 		<div className={styles.avatar}>
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZH7c1xxmppduvnIzq7oevpcDCK3nDoGgqjR6-yZ7L7kHCKDeq" />
+			<img src={props.imgSrc} />
 		</div>
 	)
 }
--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -3,9 +3,11 @@ import preloader from '../../../assets/img/preloader.svg';
 import styles from './Preloader.module.css';
 
 let Preloader = (props) => {
-    return <div className={styles.preloader}>
-        <img src={preloader} />
-    </div>
+    return (
+        <div className={styles.preloader}>
+            <img src={preloader} />
+        </div>
+    )
 }
 
 export default Preloader;
