86fbfaa5bfefdb657351cc478aa7fdffc067f9bc
SEVA@ACER <sevaaleynikov@gmail.com>
2020-07-28 13:49:25 +0300

    Updates

---
+++ precache-manifest.6d261b50a339489c60d508cd69687a91.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd77af8bb9c6a67dd22beb2f759645f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f95e3b66dbe8fed70053",
+    "url": "/react/static/css/10.25b44a53.chunk.css"
+  },
+  {
+    "revision": "dbcc81d439dc02ddaf27",
+    "url": "/react/static/css/11.0ab0ac3b.chunk.css"
+  },
+  {
+    "revision": "8d9d08d2b076854f6220",
+    "url": "/react/static/css/4.eb2b4d6e.chunk.css"
+  },
+  {
+    "revision": "1dc4d4e8e611c114736e",
+    "url": "/react/static/css/5.1afec55d.chunk.css"
+  },
+  {
+    "revision": "4a31be774088445e4358",
+    "url": "/react/static/css/6.f9fb8d04.chunk.css"
+  },
+  {
+    "revision": "01bbdc62da119092a3bb",
+    "url": "/react/static/css/7.d85125f8.chunk.css"
+  },
+  {
+    "revision": "974132dc7bf6e1b6ca51",
+    "url": "/react/static/css/8.74e616ed.chunk.css"
+  },
+  {
+    "revision": "1b69512dec43f923bdb0",
+    "url": "/react/static/css/9.2b283795.chunk.css"
+  },
+  {
+    "revision": "4864e59b45988dacfca1",
+    "url": "/react/static/css/main.89e9b81f.chunk.css"
+  },
+  {
+    "revision": "58509c66925406ac71ee",
+    "url": "/react/static/js/0.688972eb.chunk.js"
+  },
+  {
+    "revision": "f95e3b66dbe8fed70053",
+    "url": "/react/static/js/10.eb44a53b.chunk.js"
+  },
+  {
+    "revision": "dbcc81d439dc02ddaf27",
+    "url": "/react/static/js/11.d108962a.chunk.js"
+  },
+  {
+    "revision": "0fd7b6347c533b69ed64",
+    "url": "/react/static/js/3.e1a640d0.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react/static/js/3.e1a640d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9d08d2b076854f6220",
+    "url": "/react/static/js/4.fb16e338.chunk.js"
+  },
+  {
+    "revision": "1dc4d4e8e611c114736e",
+    "url": "/react/static/js/5.5d63e54d.chunk.js"
+  },
+  {
+    "revision": "4a31be774088445e4358",
+    "url": "/react/static/js/6.fa11d867.chunk.js"
+  },
+  {
+    "revision": "01bbdc62da119092a3bb",
+    "url": "/react/static/js/7.5252a6a1.chunk.js"
+  },
+  {
+    "revision": "974132dc7bf6e1b6ca51",
+    "url": "/react/static/js/8.c9a129e4.chunk.js"
+  },
+  {
+    "revision": "1b69512dec43f923bdb0",
+    "url": "/react/static/js/9.5904080e.chunk.js"
+  },
+  {
+    "revision": "4864e59b45988dacfca1",
+    "url": "/react/static/js/main.3df49159.chunk.js"
+  },
+  {
+    "revision": "c4952534cd2bd0ed7cf3",
+    "url": "/react/static/js/runtime-main.2a3e9f39.js"
+  },
+  {
+    "revision": "44330dbd1b68607423fba43ee3aad470",
+    "url": "/react/static/media/noneAvatar.44330dbd.png"
+  },
+  {
+    "revision": "52d81aaccf36e511da9cc50d449648e6",
+    "url": "/react/static/media/preloader.52d81aac.svg"
+  }
+]);
